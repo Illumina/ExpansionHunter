ded94369b2a3621938990903a2fe0093f52bc8ba
edolzhenko <edolzhenko@illumina.com>
2017-12-10 11:32:33 -0800

    EH-128: Port graph mapping

---
+++ graphs/graph_mapping.h
@@ -0,0 +1,173 @@
+//
+// Expansion Hunter
+// Copyright (c) 2016 Illumina, Inc.
+//
+// Author: Egor Dolzhenko <edolzhenko@illumina.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#pragma once
+
+#include <initializer_list>
+#include <iostream>
+#include <map>
+#include <regex>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "graphs/graph.h"
+
+class Operation {
+ public:
+  enum class Type {
+    kMatch,
+    kMismatch,
+    kInsertionToRef,
+    kDeletionFromRef,
+    kSoftClipping,
+    kMissingBases
+  };
+
+  Operation(char type_encoding, int length, std::string query,
+            std::string reference) {
+    decodeOperation(type_encoding, length, std::move(query),
+                    std::move(reference));
+    validate();
+  }
+  Operation(std::string cigar, std::string query, std::string reference);
+  Type type() const { return type_; }
+  int length() const { return length_; }
+  std::string query() const { return query_; }
+  std::string reference() const { return reference_; }
+  int32_t querySpan() const;
+  int32_t referenceSpan() const;
+  bool operator==(const Operation& other) const {
+    return type_ == other.type_ && length_ == other.length_ &&
+           query_ == other.query_ && reference_ == other.reference_;
+  }
+  char asSymbol() const;
+
+ private:
+  void decodeOperation(char op_char, int length, std::string query_seq,
+                       std::string reference_seq);
+  void validate() const;
+  Type type_;
+  int length_;
+  std::string query_;
+  std::string reference_;
+};
+
+std::ostream& operator<<(std::ostream& os, const Operation& operation);
+
+class Mapping {
+ public:
+  typedef size_t size_type;
+
+  Mapping() = default;
+  Mapping(int32_t reference_start, std::vector<Operation> operations)
+      : reference_start_(reference_start), operations_(std::move(operations)) {
+    updateCounts();
+  }
+  Mapping(int32_t reference_start, const std::string& encoding,
+          const std::string& query, const std::string& reference);
+  size_type num_operations() const { return operations_.size(); }
+  std::string query() const;
+  std::string reference() const;
+  int32_t querySpan() const;
+  int32_t referenceSpan() const;
+  int32_t reference_start() const { return reference_start_; }
+  void set_reference_start(int32_t reference_start) {
+    reference_start_ = reference_start;
+  }
+  const Operation& operator[](size_type index) const {
+    return operations_[index];
+  }
+  bool operator==(const Mapping& other) const {
+    return operations_ == other.operations_ &&
+           reference_start_ == other.reference_start_;
+  }
+  size_t matched() const { return matched_; }
+  size_t mismatched() const { return mismatched_; }
+  size_t clipped() const { return clipped_; }
+  size_t inserted() const { return inserted_; }
+  size_t deleted() const { return deleted_; }
+
+ protected:
+  void decodeOperations(int32_t reference_start, const std::string& encoding,
+                        const std::string& query, const std::string& reference);
+
+  void updateCounts();
+
+ private:
+  size_t matched_ = 0;
+  size_t mismatched_ = 0;
+  size_t clipped_ = 0;
+  size_t inserted_ = 0;
+  size_t deleted_ = 0;
+  size_t missing_ = 0;
+  int32_t reference_start_ = 0;
+  std::vector<Operation> operations_;
+};
+
+std::ostream& operator<<(std::ostream& os, const Mapping& mapping);
+
+struct NodeMapping {
+  int32_t node_id;
+  Mapping mapping;
+  bool operator==(const NodeMapping& other) const {
+    return node_id == other.node_id && mapping == other.mapping;
+  }
+};
+
+class GraphMapping {
+ public:
+  typedef size_t size_type;
+  typedef std::vector<NodeMapping> NodeMappings;
+  typedef NodeMappings::const_iterator const_iterator;
+  GraphMapping() = default;
+  GraphMapping(const std::vector<int32_t>& node_ids,
+               const std::vector<Mapping>& mappings) {
+    for (size_t index = 0; index != node_ids.size(); ++index) {
+      NodeMapping node_mapping;
+      node_mapping.node_id = node_ids[index];
+      node_mapping.mapping = mappings[index];
+      node_mappings_.push_back(node_mapping);
+    }
+  }
+  std::string query() const;
+  std::string reference() const;
+  int32_t querySpan() const;
+  int32_t referenceSpan() const;
+  const_iterator begin() const { return node_mappings_.begin(); }
+  const_iterator end() const { return node_mappings_.end(); }
+  const NodeMapping& front() const { return node_mappings_.front(); }
+  const NodeMapping& back() const { return node_mappings_.back(); }
+  size_type size() const { return node_mappings_.size(); }
+  const NodeMapping& operator[](size_t index) const {
+    return node_mappings_[index];
+  }
+
+  bool operator==(const GraphMapping& other) const {
+    return node_mappings_ == other.node_mappings_;
+  }
+  friend std::ostream& operator<<(std::ostream& os,
+                                  const GraphMapping& graph_mapping);
+
+ private:
+  NodeMappings node_mappings_;
+};
+
+std::ostream& operator<<(std::ostream& os, const GraphMapping& graph_mapping);
---
+++ graphs/unit_tests/graph_mapping_test.cc
@@ -0,0 +1,154 @@
+//
+// Expansion Hunter
+// Copyright (c) 2016 Illumina, Inc.
+//
+// Author: Egor Dolzhenko <edolzhenko@illumina.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#include "graphs/graph_mapping.h"
+
+#include "gtest/gtest.h"
+
+#include "graphs/graph.h"
+#include "graphs/graph_builders.h"
+#include "graphs/graph_mapping_operations.h"
+
+using std::string;
+using std::vector;
+
+TEST(Operation, InitializesFromString) {
+  Operation operation("3M", "ATC", "ATC");
+  Operation expected_operation('M', 3, "ATC", "ATC");
+  ASSERT_EQ(expected_operation, operation);
+}
+
+TEST(Operation, OutputsQueryAndReferenceSpans) {
+  {
+    Operation operation("3M", "AAA", "AAA");
+    EXPECT_EQ((int32_t)3, operation.querySpan());
+    EXPECT_EQ((int32_t)3, operation.referenceSpan());
+  }
+  {
+    Operation operation("4X", "AAAA", "TTTT");
+    EXPECT_EQ((int32_t)4, operation.querySpan());
+    EXPECT_EQ((int32_t)4, operation.referenceSpan());
+  }
+  {
+    Operation operation("5D", "", "AAAAA");
+    EXPECT_EQ((int32_t)0, operation.querySpan());
+    EXPECT_EQ((int32_t)5, operation.referenceSpan());
+  }
+  {
+    Operation operation("7I", "AAAAAAA", "");
+    EXPECT_EQ((int32_t)7, operation.querySpan());
+    EXPECT_EQ((int32_t)0, operation.referenceSpan());
+  }
+  {
+    Operation operation("10S", "AAAAAAAAAA", "");
+    EXPECT_EQ((int32_t)10, operation.querySpan());
+    EXPECT_EQ((int32_t)0, operation.referenceSpan());
+  }
+  {
+    Operation operation("7N", "NNNNNNN", "NNNNNNN");
+    EXPECT_EQ((int32_t)7, operation.querySpan());
+    EXPECT_EQ((int32_t)7, operation.referenceSpan());
+  }
+  {
+    Operation operation("3N", "NCN", "CNN");
+    EXPECT_EQ((int32_t)3, operation.querySpan());
+    EXPECT_EQ((int32_t)3, operation.referenceSpan());
+  }
+}
+
+TEST(Operation, ErrorsOutOnUnexpectedSequences) {
+  EXPECT_ANY_THROW(Operation("4M", "AAAA", "ATCG"));
+  EXPECT_ANY_THROW(Operation("4M", "AAAA", "ATC"));
+  EXPECT_ANY_THROW(Operation("4M", "AAA", "AAA"));
+
+  EXPECT_ANY_THROW(Operation("4N", "NNN", "NNN"));
+  EXPECT_ANY_THROW(Operation("3N", "NN", "NNN"));
+  EXPECT_ANY_THROW(Operation("2N", "NT", "NT"));
+
+  EXPECT_ANY_THROW(Operation("2X", "AT", "TT"));
+  EXPECT_ANY_THROW(Operation("2X", "AT", "A"));
+
+  EXPECT_ANY_THROW(Operation("4D", "AAA", ""));
+  EXPECT_ANY_THROW(Operation("4D", "", ""));
+
+  EXPECT_ANY_THROW(Operation("2I", "AA", "T"));
+
+  EXPECT_ANY_THROW(Operation("2S", "TTT", ""));
+  EXPECT_ANY_THROW(Operation("2S", "TT", "T"));
+}
+
+TEST(Mapping, InitializesFromCigar) {
+  // query: ---TTCGTT--TTGGGTCCCCCCCCCC
+  //           ||| ||  ||   |
+  //   ref: CCCTTCCNNAATT---T----------
+  string query = "TTCGTTTTGGGTCCCCCCCCCC";
+  string reference = "CCCTTCCNNAATTT";
+
+  Mapping mapping(3, "3M1X2N2D2M3I1M10S", query, reference);
+  const vector<Operation> operations = {
+      Operation('M', 3, "TTC", "TTC"), Operation('X', 1, "G", "C"),
+      Operation('N', 2, "TT", "NN"),   Operation('D', 2, "", "AA"),
+      Operation('M', 2, "TT", "TT"),   Operation('I', 3, "GGG", ""),
+      Operation('M', 1, "T", "T"),     Operation('S', 10, "CCCCCCCCCC", "")};
+
+  Mapping expected_mapping(3, operations);
+  ASSERT_EQ(expected_mapping, mapping);
+}
+
+TEST(Mapping, CalculatesQueryAndReferenceSpans) {
+  Mapping mapping(3, "3M1X2M2D2M3I1M10S", "TTCGTTTTGGGTCCCCCCCCCC",
+                  "CCCTTCCTTAATTT");
+  EXPECT_EQ((int32_t)22, mapping.querySpan());
+  EXPECT_EQ((int32_t)11, mapping.referenceSpan());
+}
+
+TEST(Mapping, OutputsQueryAndReferenceSequences) {
+  Mapping mapping(3, "3M1X2M2D2M3I1M10S", "TTCGTTTTGGGTCCCCCCCCCC",
+                  "CCCTTCCTTAATTT");
+  EXPECT_EQ("TTCGTTTTGGGT", mapping.query());
+  EXPECT_EQ("TTCCTTAATTT", mapping.reference());
+}
+
+TEST(GraphMapping, StitchesQueryAndReferenceSequences) {
+  Graph graph = makeDeletionGraph("AAAA", "TTGG", "TTTT");
+  const string query = "AAAATTCCC";
+  GraphMapping graph_mapping =
+      decodeFromString(0, "0[4M]1[2M3S]", query, graph);
+  EXPECT_EQ("AAAATT", graph_mapping.query());
+  EXPECT_EQ("AAAATT", graph_mapping.reference());
+}
+
+TEST(GraphMapping, CalculatesQueryAndReferenceSpans) {
+  Graph graph = makeDeletionGraph("AAAA", "TTGG", "TTTT");
+  const string query = "AAAATTCCC";
+  GraphMapping graph_mapping =
+      decodeFromString(0, "0[4M]1[2M3S]", query, graph);
+  EXPECT_EQ((int32_t)9, graph_mapping.querySpan());
+  EXPECT_EQ((int32_t)6, graph_mapping.referenceSpan());
+}
+
+TEST(GraphMapping, AllowsAccessingNodeMappingsByIndex) {
+  Graph graph = makeDeletionGraph("AAAA", "TTGC", "TTTT");
+  const string query = "AAAATTCCC";
+  GraphMapping graph_mapping =
+      decodeFromString(0, "0[4M]1[2M3S]", query, graph);
+  EXPECT_EQ(Mapping(0, "4M", "AAAA", "AAAA"), graph_mapping[0].mapping);
+  EXPECT_EQ(Mapping(0, "2M3S", "TTCCC", "TTGG"), graph_mapping[1].mapping);
+}
