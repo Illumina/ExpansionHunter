55b72b6cc57dd583f8d62b706f729dc7bf8f6619
edolzhenko <edolzhenko@illumina.com>
2017-12-14 07:50:30 -0800

    EH-131: Begin reorganization of path unit tests

--- graphs/unit_tests/path_test.cc
+++ graphs/unit_tests/path_test.cc
@@ -18,34 +18,6 @@
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //
 
-// -*- mode: c++; indent-tabs-mode: nil; -*-
-//
-// Copyright (c) 2017 Illumina, Inc.
-// All rights reserved.
-
-// Redistribution and use in source and binary forms, with or without
-// modification, are permitted provided that the following conditions are met:
-
-// 1. Redistributions of source code must retain the above copyright notice,
-// this
-//    list of conditions and the following disclaimer.
-
-// 2. Redistributions in binary form must reproduce the above copyright notice,
-//    this list of conditions and the following disclaimer in the documentation
-//    and/or other materials provided with the distribution.
-
-// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
-// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-// CONTRACT, STRICT LIABILITY, OR TORT INCLUDING NEGLIGENCE OR OTHERWISE)
-// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-// POSSIBILITY OF SUCH DAMAGE.
-
 #include "graphs/path.h"
 
 #include <list>
@@ -119,11 +91,14 @@ class DoubleSwapGraph : public ::testing::Test {
   std::shared_ptr<Graph> graph_ptr;
 };
 
-TEST_F(DeletionGraph, PathReturnsItsSequence) {
+TEST(GettingPathSequence, TypicalPathOnDeletionGraph_SequenceReturned) {
+  Graph graph = makeDeletionGraph("AAAACC", "TTTGG", "ATTT");
+  std::shared_ptr<Graph> graph_ptr = std::make_shared<Graph>(graph);
   {
     GraphPath path(graph_ptr, 3, {0}, 3);
     EXPECT_EQ("A", path.seq());
   }
+
   {
     GraphPath path(graph_ptr, 3, {1}, 4);
     EXPECT_EQ("GG", path.seq());
@@ -135,6 +110,9 @@ TEST_F(DeletionGraph, PathReturnsItsSequence) {
   }
 }
 
+TEST(GettingPathSequence, TypicalPathOnStrGraph_SequenceReturned) {}
+
+/*
 TEST_F(DeletionGraph, PathReturnsItsLength) {
   {
     GraphPath path(graph_ptr, 3, {0}, 3);
@@ -342,4 +320,4 @@ TEST_F(DoubleSwapGraph, PathExtensionsThatAreTooLongAreNotOutput) {
         GraphPath(graph_ptr, 0, {0, 1, 3, 4, 6}, 3)};
     ASSERT_EQ(expected_path_extensions, path_extensions);
   }
-}
+}*/
