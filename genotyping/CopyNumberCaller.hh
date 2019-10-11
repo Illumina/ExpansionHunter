e38608e39e22892d9714863a943e9e5ec2b3b4cb
Xiao Chen <chenxecho@gmail.com>
2019-10-11 08:59:13 -0700

    Call final copy number change for CNV (#67)
    Consider baseline regions in CNV calling

---
+++ genotyping/CopyNumberCaller.hh
@@ -0,0 +1,39 @@
+//
+// Expansion Hunter
+// Copyright 2016-2019 Illumina, Inc.
+// All rights reserved.
+//
+// Author: Xiao Chen <xchen2@illumina.com>
+//         Egor Dolzhenko <edolzhenko@illumina.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//
+
+#pragma once
+
+#include <boost/optional.hpp>
+#include <vector>
+
+#include "common/Common.hh"
+
+namespace ehunter
+{
+boost::optional<int> callCopyNumberForOverlappingCNV(
+    boost::optional<int> targetCopyNumber, const std::vector<boost::optional<int>>& baselineCopyNumbers,
+    int expectedBaselineCopyNumber);
+
+boost::optional<int> callCopyNumberForNonOverlappingCNV(
+    boost::optional<int> targetCopyNumber, const std::vector<boost::optional<int>>& baselineCopyNumbers,
+    int expectedBaselineCopyNumber);
+}
